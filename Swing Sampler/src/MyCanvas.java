bacaf169768f2d8b1e0783f1d9386fe0fb9150e5
jonathan.aragon <jonathan.aragon@mu-c1-16.mullen.pvt.k12.co.us>
2019-10-17 08:22:30 -0600

    Got challenge 2.1

--- Swing Sampler/src/MyCanvas.java
+++ Swing Sampler/src/MyCanvas.java
@@ -1,46 +1,57 @@
+
 import java.awt.*;
+import java.util.Scanner;
 
 import javax.swing.*;
 //COMMENT CMOMENT COMMENT CMOMENT
 
 public class MyCanvas extends Canvas
 	{
-		private static final long serialVersionUID = 1L;
+		private static final long	serialVersionUID	= 1L;
+		private static Scanner		heightC;
+		private static Scanner		widthC;
+		private static Scanner		colorC;
+		static int					height;
+		static int					width;
+		static int					color;
 		
 		public static void main(String[] args)
 			{
 				MyCanvas canvas = new MyCanvas();
 				JFrame frame = new JFrame();
-				frame.setSize(500, 400);
+				frame.setSize(750, 750);
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.getContentPane().add(canvas).setBackground(Color.white);
 				frame.setLocationRelativeTo(null); // Sets JPanel to center of screen
-				frame.setResizable(false);
+				frame.setResizable(true);
 				frame.setVisible(true);
 			}
 			
 		public void paint(Graphics graphics)
 			{
-				
-				graphics.setColor(Color.red);
-				graphics.fillOval(140, 130, 100, 100);
-				
-				graphics.setColor(Color.blue);
-				graphics.fillRect(10, 10, 200, 100);
-				
-				graphics.setColor(Color.black);
-				graphics.drawLine(40, 30, 330, 380);
-				
-				for (int i = 0; i < 350; i++)
+				System.out.println("How wide do you want your rectangle to be?");
+				widthC = new Scanner(System.in);
+				width = widthC.nextInt();
+				System.out.println("How tall do you want your rectagle to be?");
+				heightC = new Scanner(System.in);
+				height = heightC.nextInt();
+				System.out.println("Do you want it to be red or blue? 0 for red and 1 for blue.");
+				colorC = new Scanner(System.in);
+				color = colorC.nextInt();
+				if (color == 1)
 					{
-						graphics.setColor(Color.green);
-						graphics.fillRect(300, i, 50, 50);
-						
-						delay();
-						
-						graphics.setColor(Color.white);
-						graphics.fillRect(300, i, 50, 50);
+						graphics.setColor(Color.blue);
 					}
+				else if (color == 0)
+					{
+						graphics.setColor(Color.red);
+					}
+				else
+					{
+						System.out.println("It's going to be a fresh start Morty");
+						System.exit(0);
+					}
+				graphics.fillRect(375 - (width / 2), 375 - (height / 2), width, height);
 			}
 			
 		public void delay()
